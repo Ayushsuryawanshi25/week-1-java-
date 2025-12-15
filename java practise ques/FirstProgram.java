d98e72912ef886a53cd75b1741f66ca51e27fe48
Ayush Suryawanshi <ayushsuryawanshi2507@gmail.com>
2025-12-15 21:00:51 +0530

    Adding FirstProgram

---
+++ java practise ques/FirstProgram.java
@@ -0,0 +1,9 @@
+//write a program to print bridge lab in the screen?
+ 
+import java.util.*;
+
+public class FirstProgram{
+public static void main (String [] args){
+System.out.println("Hello! bridge labs");
+}
+}
